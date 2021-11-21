6e080ada185f2135c3405b1ac2b094e602bc3445
Trevor Covington <tcovington189@gmail.com>
2021-11-21 08:55:06 -0800

    Adding scrolling logic

--- src/GroceryList/EditGroceryList.jsx
+++ src/GroceryList/EditGroceryList.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 
-import { ChevronLeftIcon, PlusSmIcon } from '@heroicons/react/outline'
+import { ChevronLeftIcon, PlusSmIcon, ArrowCircleUpIcon } from '@heroicons/react/outline'
 import GroceryEditListItem from './GroceryEditListItem';
 import Button from '../global/Button';
 import dataProvider from '../data/dataProvider';
@@ -19,10 +19,17 @@ function EditGroceryList() {
         }
     }, [currentList]);
 
+    useEffect(() => {
+        if (listItems.length) {
+            const lastItemRow = document.querySelector('.item-row:last-child');
+            lastItemRow.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [listItems]);
 
     if (!currentList) {
         return null;
     }
+
 
     function addNewItemHandler() {
         const updatedList = dataProvider.addNewListItem(id);
@@ -38,6 +45,10 @@ function EditGroceryList() {
         setListItems(updatedList.items);
     }
 
+    function backToTop() {
+        document.body.scrollIntoView({ behavior: "smooth" })
+    }
+
     return (
         <div>
             <div className="flex items-center">
@@ -49,14 +60,15 @@ function EditGroceryList() {
                 <p className="font-bold text-xl flex-1">{currentList.name}</p>
             </div>
 
-            <div className="flex flex-col mt-5">
+            <div className="flex flex-col mt-5 mb-12 item-list">
                 {listItems.length === 0 && <p className="mt-5">No items yet.</p>}
 
                 {listItems.map((listItem, index) => <GroceryEditListItem deleteHandler={deleteHandler} listItem={listItem} key={`list_item_${index}`} />)}
 
-                <div className="flex mt-8 justify-end">
-                    <Button type="success" clickHandler={addNewItemHandler}><PlusSmIcon className="w-5" /> Add Item</Button>
-                </div>
+            </div>
+            <div className="flex mt-8 justify-between fixed bottom-2 right-2 left-2">
+                <Button type="primary" clickHandler={backToTop}><ArrowCircleUpIcon className="w-5" /></Button>
+                <Button type="success" clickHandler={addNewItemHandler}><PlusSmIcon className="w-5" /> Add Item</Button>
             </div>
         </div>
     )
--- src/GroceryList/GroceryEditListItem.jsx
+++ src/GroceryList/GroceryEditListItem.jsx
@@ -4,34 +4,37 @@ import { TrashIcon, SaveIcon } from '@heroicons/react/outline'
 
 import Button from '../global/Button';
 
-function GroceryEditListItem({ listItem, deleteHandler }) {
+function GroceryEditListItem({ listItem, deleteHandler, updateHandler }) {
     const [quantity, setQuantity] = useState(listItem.quantity ?? 0);
     const [item, setItem] = useState(listItem.item ?? "");
 
     return (
-        <div className="flex items-end space-x-2 mb-2">
-            <div className="w-1/6 mr-2">
-                <label htmlFor={`quantity_${listItem.id}`}>Quantity: </label>
-                <input
-                    id={`quantity_${listItem.id}`}
-                    defaultValue={quantity}
-                    onChange={(e) => setQuantity(e.target.value)}
-                    className="border border-gray-400 p-1 mt-1 rounded-sm w-full"
-                    type="number"
-                />
+        <div className="flex flex-col items-end space-x-2 mb-2 item-row">
+            <div className="flex mb-1">
+                <div className="w-1/6 mr-2">
+                    <label htmlFor={`quantity_${listItem.id}`}>Qty: </label>
+                    <input
+                        id={`quantity_${listItem.id}`}
+                        defaultValue={quantity}
+                        onChange={(e) => setQuantity(e.target.value)}
+                        className="border border-gray-400 p-1 mt-1 rounded-sm w-full"
+                        type="number"
+                    />
+                </div>
+                <div className="w-5/6">
+                    <label htmlFor={`item_${listItem.id}`}>Item: </label>
+                    <input
+                        id={`item_${listItem.id}`}
+                        defaultValue={item}
+                        onChange={(e) => setItem(e.target.value)}
+                        className="border border-gray-400 p-1 mt-1 rounded-sm w-full"
+                        type="text"
+                    />
+                </div>
             </div>
-            <div className="w-5/6">
-                <label htmlFor="item">Item: </label>
-                <input
-                    id="item"
-                    defaultValue={item}
-                    onChange={(e) => setItem(e.target.value)}
-                    className="border border-gray-400 p-1 mt-1 rounded-sm w-full"
-                    type="text"
-                />
-            </div>
-            <div className="w-2/6 flex justify-between">
-                <Button type="primary" clickHandler={() => deleteHandler(listItem.id)}>
+
+            <div className="flex justify-between space-x-2">
+                <Button type="primary" clickHandler={() => updateHandler({ id: listItem.id, item, quantity })}>
                     <SaveIcon className="w-5" />
                 </Button>
 
