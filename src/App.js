83eaadc3f5f679c413691d2e0dd1e53cd1b195d7
Trevor Covington <tcovington189@gmail.com>
2021-11-21 10:23:34 -0800

    Styling

--- src/App.js
+++ src/App.js
@@ -43,9 +43,9 @@ function App() {
 
   return (
     <div className="flex flex-col min-h-screen bg-gray-300">
-      <header className="w-full bg-gray-500 text-gray-50 py-3 px-2">
+      <header className="w-full bg-gray-500 text-gray-50 py-3 px-2 h-12">
         <div className="flex">
-          <ShoppingCartIcon className="w-5 mr-1" /> Grocery List App
+          Grocery List App
         </div>
       </header>
       <main className="my-2 px-2 flex-1">
