a2958e565bf5f04e9585ef185e6e4dc73a65fccc
Trevor Covington <tcovington189@gmail.com>
2021-11-27 07:04:24 -0800

    Updating modal styling

--- src/global/Modal.jsx
+++ src/global/Modal.jsx
@@ -11,8 +11,8 @@ function Modal({
     useOutsideClicked(modalRef, closeHandler)
 
     return (
-        <div className="absolute top-0 right-0 bottom-0 left-0 py-5" style={{ backgroundColor: "rgba(0,0,0,.3" }}>
-            <div ref={modalRef} className="bg-gray-300 w-5/6 mx-auto py-2 px-2 shadow">
+        <div className="absolute top-0 right-0 bottom-0 left-0 pt-12" style={{ backgroundColor: "rgba(0,0,0,.4" }}>
+            <div ref={modalRef} className="bg-gray-300 w-5/6 mx-auto py-2 px-3 shadow-lg rounded-sm border-gray-400 border">
                 <div className="flex justify-between items-center mb-2">
                     <p className="font-bold">{modalTitle}</p>
                     <XIcon className="w-5" onClick={() => closeHandler()} />
