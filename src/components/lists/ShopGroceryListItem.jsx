e2c0bfd54a10b00f470fb810742d0974008038d1
Trevor Covington <tcovington189@gmail.com>
2021-12-05 19:29:10 -0800

    Made style update more obvious

--- src/components/lists/ShopGroceryListItem.jsx
+++ src/components/lists/ShopGroceryListItem.jsx
@@ -1,28 +1,38 @@
-import { ShoppingBagIcon } from '@heroicons/react/outline'
-import React, { useState } from 'react'
-import Button from '../../global/Button';
+import { ShoppingBagIcon } from "@heroicons/react/outline";
+import React, { useState } from "react";
+import Button from "../../global/Button";
 
+function ShopGroceryListItem({ listId, listItem, updateHandler }) {
+  const [item, setItem] = useState(listItem ?? "");
 
-function ShopGroceryListItem({listId, listItem, updateHandler }) {
-    const [item, setItem] = useState(listItem ?? "");
+  const [acquired, setAcquired] = useState(item.acquired ?? false);
 
-    const [acquired, setAcquired] = useState(item.acquired ?? false);
+  function onChangeHandler() {
+    const acquiredStatus = !acquired;
+    setAcquired(acquiredStatus);
+    setItem({ ...item, acquired: acquiredStatus });
+    updateHandler(listId, { id: listItem.id, acquired: acquiredStatus });
+  }
 
-    function onChangeHandler() {
-        const acquiredStatus = !acquired;
-        setAcquired(acquiredStatus);
-        setItem({...item, acquired: acquiredStatus});
-        updateHandler(listId, { id: listItem.id, acquired: acquiredStatus })
-    }
+  let itemStyle = {
+    textDecoration: acquired ? "line-through" : null,
+    fontStyle: acquired ? "italic" : null,
+    color: acquired ? 'rgba(0,0,0,.5)' : null,
+  };
 
-    return (
-        <div className="flex items-center space-x-3 mb-4" style={{ textDecoration: acquired ? 'line-through' : null }}>
-            <Button type="primary" clickHandler={() => onChangeHandler()}>
-                <ShoppingBagIcon className="w-4" />
-            </Button>
-            <p>{item.quantity} {item.item}</p>
-        </div>
-    )
+  return (
+    <div
+      className="flex items-center space-x-3 mb-4"
+      style={itemStyle}
+    >
+      <Button type="primary" clickHandler={() => onChangeHandler()}>
+        <ShoppingBagIcon className="w-4" />
+      </Button>
+      <p>
+        {item.quantity} {item.item}
+      </p>
+    </div>
+  );
 }
 
-export default ShopGroceryListItem
+export default ShopGroceryListItem;
--- src/views/ShopGroceryList.jsx
+++ src/views/ShopGroceryList.jsx
@@ -1,82 +1,108 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
-import { ChevronLeftIcon } from '@heroicons/react/outline'
+import { ChevronLeftIcon } from "@heroicons/react/outline";
 
-import dataProvider from '../data/dataProvider';
-import ShopGroceryListItem from '../components/lists/ShopGroceryListItem';
-
-
+import dataProvider from "../data/dataProvider";
+import ShopGroceryListItem from "../components/lists/ShopGroceryListItem";
+import categoryDataProvider from "../data/categoryDataProvider";
 
 function ShopGroceryList() {
-    const { id } = useParams();
+  const { id } = useParams();
 
-    const [currentList, setCurrentList] = useState(dataProvider.getList(id));
-    const [listItems, setListItems] = useState([]);
+  const [currentList, setCurrentList] = useState(dataProvider.getList(id));
+  const [listItems, setListItems] = useState([]);
 
-    const [visibleCategory, setVisibleCategory] = useState('all');
+  const allCategories = categoryDataProvider.getCategories();
 
-    useEffect(() => {
-        if (currentList) {
-            formatGroceryListItems(currentList);
-        }
-    }, [currentList]);
+  const [visibleCategory, setVisibleCategory] = useState("all");
 
-    let allCategories = currentList.categories;
-
-    function updateHandler(listId, item) {
-        const updatedList = dataProvider.updateListItem(listId, item);
-
-        setCurrentList(updatedList);
-        formatGroceryListItems(updatedList);
+  useEffect(() => {
+    if (currentList) {
+      formatGroceryListItems(currentList);
     }
+  }, [currentList]);
 
-    function formatGroceryListItems(list) {
-        const categories = list.categories;
+  function updateHandler(listId, item) {
+    const updatedList = dataProvider.updateListItem(listId, item);
 
-        const groceryItems = categories.map((category) => ({
-            category,
-            items: list.items.filter((item) => item.category === category)
-        }));
+    setCurrentList(updatedList);
+    formatGroceryListItems(updatedList);
+  }
 
-        setListItems(groceryItems);
-    }
+  function formatGroceryListItems(list) {
+    const categories = allCategories;
 
-    return (
-        <div>
-            <div className="flex items-center">
-                <div className="w-1/6">
-                    <Link to="/">
-                        <ChevronLeftIcon className="w-6" />
-                    </Link>
-                </div>
-                <p className="font-bold text-xl w-5/6">Shopping {currentList.name}</p>
-            </div>
-            <div className="flex mt-4">
-                <label className="pr-2 font-bold" htmlFor="categories">Filter:</label>
-                <select className="bg-gray-200 shadow-sm py-1 pl-1" name="categories" id="categories" value={visibleCategory} onChange={(e) => setVisibleCategory(e.target.value)}>
-                    <option value="all">All</option>
-                    {
-                        allCategories.map((cat, index) => <option value={cat} key={`category_${index}`}>{cat}</option>)
-                    }
-                </select>
-            </div>
-            <div className="flex flex-col mb-12 item-list">
-                {listItems.length === 0 && <p className="mt-5">No items yet.</p>}
+    const groceryItems = categories.map((category) => ({
+      category,
+      items: list.items.filter((item) => item.category === category),
+    }));
 
-                {listItems.map((listItem, index) =>
-                    <div className={`pb-6 ${listItem.category === visibleCategory || visibleCategory === 'all' ? 'flex flex-col' : 'hidden' }`} key={`list_category_${index}`}>
-                        <p className="font-bold text-right">{listItem.category}</p>
-                        <hr className="border-gray-400 mb-2" />
-                        {listItem.items.map((item, index_2) =>
-                             <ShopGroceryListItem listId={id} updateHandler={updateHandler} listItem={item} key={`list_item_${index_2}`} />)}
-                    </div>
-                )}
+    setListItems(groceryItems);
+  }
 
-            </div>
+  return (
+    <div>
+      <div className="flex items-center">
+        <div className="w-1/6">
+          <Link to="/">
+            <ChevronLeftIcon className="w-6" />
+          </Link>
         </div>
-    )
+        <p className="font-bold text-xl w-5/6">Shopping {currentList.name}</p>
+      </div>
+      <div className="flex mt-4">
+        <label className="pr-2 font-bold" htmlFor="categories">
+          Filter:
+        </label>
+        <select
+          className="bg-gray-200 shadow-sm py-1 pl-1"
+          name="categories"
+          id="categories"
+          value={visibleCategory}
+          onChange={(e) => setVisibleCategory(e.target.value)}
+        >
+          <option value="all">All</option>
+          {allCategories.map((cat, index) => (
+            <option value={cat} key={`category_${index}`}>
+              {cat}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="flex flex-col mb-12 item-list">
+        {listItems.length === 0 && <p className="mt-5">No items yet.</p>}
+
+        {listItems.map((listItem, index) => {
+          return (
+            listItem.items.length > 0 && (
+              <div
+                className={`pb-6 ${
+                  listItem.category === visibleCategory ||
+                  visibleCategory === "all"
+                    ? "flex flex-col"
+                    : "hidden"
+                }`}
+                key={`list_category_${index}`}
+              >
+                <p className="font-bold text-right">{listItem.category}</p>
+                <hr className="border-gray-400 mb-2" />
+                {listItem.items.map((item, index_2) => (
+                  <ShopGroceryListItem
+                    listId={id}
+                    updateHandler={updateHandler}
+                    listItem={item}
+                    key={`list_item_${index_2}`}
+                  />
+                ))}
+              </div>
+            )
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default ShopGroceryList
+export default ShopGroceryList;
